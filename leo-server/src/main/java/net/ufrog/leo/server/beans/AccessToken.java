c2fd0177744499c51d5ee2d53ced576e46090d4c
ultrafrog <ufrog.net@gmail.com>
2017-03-08 21:54:43 +0800

    好无聊啊好无聊 感觉没什么头绪啊

--- leo-server/src/main/java/net/ufrog/leo/server/beans/AccessToken.java
+++ leo-server/src/main/java/net/ufrog/leo/server/beans/AccessToken.java
@@ -3,13 +3,12 @@ package net.ufrog.leo.server.beans;
 import java.io.Serializable;
 
 /**
- * 访问令牌
+ * 访问令牌接口
  *
  * @author ultrafrog, ufrog.net@gmail.com
  * @version 0.1, 2017-02-26
  * @since 0.1
  */
-public class AccessToken implements Serializable {
+public interface AccessToken extends Serializable {
 
-    private static final long serialVersionUID = -631864410517042460L;
 }
