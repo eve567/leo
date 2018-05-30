a4cdb99f36004983580358501a3532361d7df4a1
ultrafrog <ufrog.net@gmail.com>
2018-05-30 14:31:52 +0800

    Emm...

--- leo-server/src/main/java/net/ufrog/leo/server/controllers/UserController.java
+++ leo-server/src/main/java/net/ufrog/leo/server/controllers/UserController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Service;
 import java.util.stream.Collectors;
 
 /**
