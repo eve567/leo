a5d391dd30de6c5798cefca0d9c718991d3a1002
ultrafrog <ufrog.net@gmail.com>
2018-08-23 17:48:41 +0800

    修正用户邮件查询为[_null]时的问题

--- leo-service/src/main/java/net/ufrog/leo/service/impls/UserServiceImpl.java
+++ leo-service/src/main/java/net/ufrog/leo/service/impls/UserServiceImpl.java
@@ -121,7 +121,7 @@ public class UserServiceImpl implements UserService {
         // TODO 还是要有队列来处理这些东西
         if (!Strings.empty(user.getAccount()) && userRepository.findByAccount(user.getAccount()) != null) {
             throw new ServiceException("account '" + user.getAccount() + "' exists.", "service.create.failure.exists.account");
-        } if (!Strings.empty(user.getEmail()) && userRepository.findByEmail(user.getEmail()) != null) {
+        } if (!Strings.empty(user.getEmail()) && !Strings.equals(Model.NULL, user.getEmail()) && userRepository.findByEmail(user.getEmail()) != null) {
             throw new ServiceException("email '" + user.getEmail() + "' exists.", "service.create.failure.exists.email");
         }
         return userRepository.save(user);
